dd384a729f5a5c6f06a53b754d39ce3a0c9b187a
Hossein <smh.miraftabi1373@gmail.com>
2019-06-15 22:12:06 +0100

    implement difficulty eum

---
+++ src/main/java/hossein/springframework/model/Difficulty.java
@@ -0,0 +1,6 @@
+package hossein.springframework.model;
+
+public enum Difficulty {
+
+    EASY, MODERATE, KIND_OF_HARD, HARD
+}
--- src/main/java/hossein/springframework/model/Recipe.java
+++ src/main/java/hossein/springframework/model/Recipe.java
@@ -16,7 +16,9 @@ public class Recipe {
     private String source;
     private String url;
     private String directions;
-    //    private Difficulty difficulty;
+
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
 
     @Lob
     private Byte[] image;
@@ -89,6 +91,14 @@ public class Recipe {
 
     public void setDirections(String directions) {
         this.directions = directions;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
     }
 
     public Byte[] getImage() {
